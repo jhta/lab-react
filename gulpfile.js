0d9e907d675cf36785b70a7ac79f2d96db111d43
jhta <jeisonhs93@gmail.com>
2016-02-20 11:39:38 -0500

    livereloading

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,59 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
-var babelify = require('babelify');
+var watchify = require('watchify');
+var browserSync = require('browser-sync').create();
 
-var entryPoint = 'js/index.js';
-gulp.task('build', function() {
-  return browserify({
-    entries: [entryPoint], //puntos de entrada donde corre borwserify
-    debug: true, //permite ebugger
-    cache: {},
-    packageCache: {},
-    fullPaths: false, 
-    extensions: ['.jsx'] //posibles extensiones
+var entryPointJS = './src/js/app.js'
+
+//funcion para transpilar el codigo
+//recibe un objeto de browserify con sus opciones
+function bundle(bundler) {
+  return bundler
+    .transform('babelify', {presets: ['es2015', 'react']}) //transforma a es6 con babel
+    .bundle() //constuye o transpila el codigo
+    .on('error', function(e) { //verifica errores
+      gutil.log(gutil.colors.red(e));
     })
-  .transform('babelify', {presets: ['es2015', 'react']}) //transforma a los presets indicados
-  .bundle() //compileme
-  .on('err', function(err) { //detecte errores
-    gutil.log(err)
-  })
-  .pipe(source('bundle.js'))
-  .pipe(gulp.dest('./dist/'))
-});
+    .pipe(source('bundle.js')) //nombre
+    .pipe(gulp.dest('./dist')) //destino
+    .pipe(browserSync.stream()) //emitiendo que cambio
+    .on('end', function() { //finalizo el cambio
+      return gutil.log(gutil.colors.green('bundle: OK'));
+    })
+}
 
+//opciones de browserify
+var browserifySettings = {
+  entries: [entryPointJS],
+  debug: true,
+  cache: {},
+  packageCache: {},
+  fullPaths: false,
+  extensions: ['.jsx'],
+};
+
+//Tarea para compilar js con browserify
+gulp.task('build', function() {
+    return bundle(browserify(browserifySettings));
+  }
+);
+
+// Observa constantemente los cambios en los archivos mencionados
 gulp.task('watch', function() {
-  gulp.watch('js/**/**.*', ['build']);
+  //watcher recibe objeto broserify con opcioens
+  var watcher = watchify(browserify(browserifySettings))
+  bundle(watcher);
+  watcher.on('update', function() {
+    bundle(watcher)
+  })
+  watcher.on('log', gutil.log);
+
+  browserSync.init({
+    server: './',
+    logFileChanges: false
+    });
 });
 
-gulp.task('default',['watch'])
+// Tarea que corre por defecto, en este caso watch
+gulp.task('default', ['watch']);
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import Calc from './calc';
 
 console.log("hola mundo 3");
-console.log("suma",Calc.sum(2,3));
+console.log("suma",Calc.sum(4,9));
 console.log("mult",Calc.mult(2,3));
 console.log("sub",Calc.sub(2,3));
